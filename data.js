79a5ca270727e8c21a1646e298fedc87aa03ab10
DERECH OLAM MINISTRIES INT'L <lastwarners2024@gmail.com>
2025-12-11 12:17:10 +0200

    Update data.js with correct article number

--- data.js
+++ data.js
@@ -2,5 +2,5 @@ const DOWS_DATA = {
   last_date_used: "2025-11-01",
   last_URL_processed: "https://www.prophecynewswatch.com/article.cfm?recent_news_id=9052",
   current_date: "2025-11-25",
-  last_article_number: 9052
+  last_article_number: 9175
 };
